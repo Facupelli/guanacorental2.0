f6a17110d0f2806d8c8d2ac9c7bac38bfe07573e
Facundo Pellicer <facundopellicer4@gmail.com>
2023-06-26 16:03:22 -0300

    eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -38,6 +38,8 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
   },
 };
 
