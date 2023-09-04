9bb34ff3e8ccfbe1741177aacf1b92d872a10eff
Facundo Pellicer <facundopellicer4@gmail.com>
2023-09-04 14:09:58 -0300

    fix search by number when filter is next orders

--- src/server/api/routers/order.ts
+++ src/server/api/routers/order.ts
@@ -356,10 +356,9 @@ export const orderRouter = createTRPCRouter({
 
       if (sort === ADMIN_ORDERS_SORT["NEXT ORDERS"]) {
         query.orderBy = { book: { start_date: "asc" } };
-        query.where = {
-          book: {
-            start_date: { gte: dayjs().startOf("day").toDate() },
-          },
+        query.where = {};
+        query.where.book = {
+          start_date: { gte: dayjs().startOf("day").toDate() },
         };
       }
 
